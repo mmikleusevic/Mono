313c3646f092d163a45e22386290d527fd15f2a5
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-07-18 18:19:04 +0200

    Fixed missing solution error

--- Mono.Service/Services/VehicleMakeService.cs
+++ Mono.Service/Services/VehicleMakeService.cs
@@ -1,8 +1,33 @@
 ﻿using Mono.Service.Services.Interfaces;
+using Mono.SharedLibrary;
 
 namespace Mono.Service.Services
 {
     public class VehicleMakeService : IVehicleMakeService
     {
+        public void CreateVehicleMake(VehicleMake vehicleMake)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVehicleMake(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<VehicleMakeViewModel>> GetAllVehicleMakes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleMakeViewModel> GetVehicleMake(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVehicleMake(VehicleMake vehicleMake)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Mono.Service/Services/VehicleModelService.cs
+++ Mono.Service/Services/VehicleModelService.cs
@@ -1,8 +1,33 @@
 ﻿using Mono.Service.Services.Interfaces;
+using Mono.SharedLibrary;
 
 namespace Mono.Service.Services
 {
     public class VehicleModelService : IVehicleModelService
     {
+        public void CreateVehicleModel(VehicleModel vehicleModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVehicleModel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<VehicleModelViewModel>> GetAllVehicleModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleModelViewModel> GetVehicleModel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVehicleModel(VehicleModel vehicleModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
